90bc7b6f4cc4e6d7c70f87c186c06e733a86d7e9
Esther Walker <equestriennesrule@yahoo.com>
2020-02-26 15:34:30 -0800

    added values to my columns

--- db/schema.sql
+++ db/schema.sql
@@ -4,7 +4,7 @@ CREATE DATABASE burgers_db;
 
 USE burgers_db;
 
-CREATE TABLE burgers_db (
+CREATE TABLE burgers (
     id INTEGER AUTO_INCREMENT,
     burger_name VARCHAR(30),
     devoured BOOLEAN DEFAULT false,
---
+++ db/seeds.sql
@@ -0,0 +1,10 @@
+INSERT INTO burgers (burger_name, devoured)
+VALUES ('cheeseburger', true);
+
+INSERT INTO burgers (burger_name, devoured)
+VALUES ('baconburger', true);
+
+INSERT INTO burgers (burger_name)
+VALUES ('bbqburger');
+
+SELECT * FROM burgers
