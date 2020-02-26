ddf5a2111c80e9e1329413451db38f54e11280db
Esther Walker <equestriennesrule@yahoo.com>
2020-02-26 15:17:39 -0800

    added info into my schema file

---
+++ db/schema.sql
@@ -0,0 +1,12 @@
+DROP DATABASE burgers_db;
+
+CREATE DATABASE burgers_db;
+
+USE burgers_db;
+
+CREATE TABLE burgers_db (
+    id INTEGER AUTO_INCREMENT,
+    burger_name VARCHAR(30),
+    devoured BOOLEAN DEFAULT false
+
+);
