9edd10edd68e03ed6172ec5b6ecb3ffaf34d3480
Esther Walker <equestriennesrule@yahoo.com>
2020-02-26 18:32:19 -0800

    added insertone and update one functions

--- config/orm.js
+++ config/orm.js
@@ -1,17 +1,37 @@
 const connection = require('./connection');
 
 const orm = {
-    selectAll: function(tableInput, colToSearch, valOfCol) {
-        const queryString = 'SELECT * FROM ?? WHERE ?? = ?';
-        connection.query(queryString, [tableInput, colToSearch, valOfCol], function(
+    selectAll: function(burgers, devourable) {
+        const queryString = 'SELECT * FROM ?? WHERE ??? = ?';
+        connection.query(queryString, [burgers, devourable], function(
             err,
             data
         ) {
             if (err) throw err;
             console.log(data);
-        }
-        );
+        });
+    },
+    insertOne: function(addBurger, devourable){
+        const queryString = 'INSERT INTO burgers VALUES ?';
+        connection.query(queryString, [addBurger, devourable], function(
+            err,
+            data
+        ){
+            if (err) throw err;
+            console.log(data);
+        });
+    },
+    updateOne: function(updateBurger, devoured){
+        const queryString = 'INSERT INTO burgers VALUES ?';
+        connection.query(queryString, [updateBurger, devoured], function(
+            err,
+            data
+        ){
+            if (err) throw err;
+            console.log(data)
+        });
     }
 };
+
 
 module.exports.orm;
