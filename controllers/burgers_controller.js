ee6b328f4e550a20b9ee70ac924b3db72e485f92
Esther Walker <equestriennesrule@yahoo.com>
2020-02-28 15:39:48 -0800

    added code into server

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -2,4 +2,20 @@ const express = require('express');
 
 const burger = require('../models/burger');
 
-let router = express.Router()
+let router = express.Router();
+
+router.get('/', (req, res) => {
+    burger.selectAll(function(data) {
+        const obj = {
+            burger: data
+        };
+        console.log(obj);
+        res.render('index', obj)
+    });
+});
+
+// router.post('/burger/create', (req, res) => {
+//     burger.insertOne(req.body.burger_name, (result)=> {
+//     res.json()
+//     })
+// })
--- server.js
+++ server.js
@@ -1,1 +1,26 @@
 const express = require('express');
+
+const PORT = process.env.PORT || 3000;
+
+const app = express();
+
+app.use(express.static('public'));
+
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
+
+const expressHandlebars = require('express-handlebars')
+;
+app.engine('handlebars', expressHandlers({
+    defaultLayout: 'main'}));
+    app.set('view engine', 'handlebars');
+
+    const routes = require('./controllers/burgers_controller.js');
+
+    app.use(routes);
+
+    app.listen(PORT, () =>
+    console.log(`Server listening on: http://localhost:${PORT}`)
+    );
+
+
