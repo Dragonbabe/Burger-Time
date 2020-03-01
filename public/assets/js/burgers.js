b68934824f44accbe33194074ac9ffd31e3c8463
Esther Walker <equestriennesrule@yahoo.com>
2020-02-29 18:06:14 -0800

    added functionality to my burgers.js file

---
+++ public/assets/js/burgers.js
@@ -0,0 +1,38 @@
+'use strict';
+
+$.ajax('.createburger').on('submit', function(event){
+    event.preventDefault();
+
+    const newBurger = {
+        name: $('#burger')
+        .val()
+        .trim()
+    };
+    $.ajax('/burgers/new', {
+        type: 'POST',
+        data: newBurger
+    }).then(function(){
+        console.log('created new burger!');
+        location.reload();
+    });
+});
+
+$(function(){
+    $('.updateburger').on('click', function(event){
+        const id = $(this).data('id');
+        const devoured = $(this).data('devoured');
+        
+        const newDevouredBurger = {
+
+            burger: devoured
+        };
+
+        $.ajax('/burgers/devoured' + id, {
+            type: 'PUT',
+            data: newDevouredBurger
+        }).then(function(){
+            console.log('Devoured the burger!', devoured);
+            location.reload();
+        });
+    });
+});
